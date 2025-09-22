508dfd5a4a3be05fddfcbc692af86cb184fdc5af
Dmitrii-Victorovich <dim.hohrin001@gmail.com>
2025-09-23 00:29:13 +0700

    Configure site/base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://Dmitrii-Victorovich.github.io',
+  base: '/juggernautcr-site',
+});
