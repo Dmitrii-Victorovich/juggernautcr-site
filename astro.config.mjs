8ac352435557b1f84af68dacdbb29b19249091a6
Dmitrii-Victorovich <dim.hohrin001@gmail.com>
2025-09-23 11:28:04 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 export default defineConfig({
   site: 'https://juggernautcr.ru',
-  // output: 'static', // (по умолчанию у Astro 5 уже static, можно не указывать)
-  // base: '/',         // не обязателен при корне домена
 });
