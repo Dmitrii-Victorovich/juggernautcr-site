1e5774fd6e23204dcd0807410d71741b95d1db29
Dmitrii-Victorovich <dim.hohrin001@gmail.com>
2025-09-28 22:04:21 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://juggernautcr.ru',
   base: '/',
-  integrations: [preact(), react()],
+  integrations: [preact()],
 });
-
